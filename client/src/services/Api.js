2e88e47469ddfca41bbcb03e385060824c084ce0
Татьяна Гашева <tg@get-net.ru>
2022-03-10 07:55:49 +0300

    feat: update axios fetch

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -1,7 +1,14 @@
 import axios from 'axios';
+import store from '@/store/index';
 
-export default {
-    async getCurrencies() {
-        return axios('https://www.cbr-xml-daily.ru/daily_json.js');
-    },
-};
+const provider = axios.create({
+    baseURL: process.env.VUE_APP_CURRENCY_PROVIDER,
+});
+
+provider.interceptors.response.use(null, error => {
+    console.log(error);
+    store.commit('SET_ERROR', error?.response?.data);
+    store.commit('CLEAR_CACHE');
+});
+
+export default provider;
--- client/src/store/currency.js
+++ client/src/store/currency.js
@@ -1,4 +1,4 @@
-import Api from '@/services/Api';
+import provider from '@/services/Api';
 
 export default {
     state:     {
@@ -35,25 +35,26 @@ export default {
     actions:   {
         async updateCurrencies({commit, getters}) {
             try {
-                // проверка кэша
-                // если даты совпадают и кэш существует, то обращение к кэшу
+                // checking cache
+                // if dates coincide & cache exists then take from cache
                 if (getters.currencies && getters.lastDateUpdate && (new Date(getters.lastDateUpdate)).toDateString() === new Date().toDateString()) {
                     console.log('Cash is relevant');
                 } else {
-                    // запрос в случае отсутствия\нерелевантности кэша
+                    // if cache is irrelevant
                     commit('CLEAR_CACHE');
-                    let newCurrencies = Object.values((await Api.getCurrencies()).data.Valute);
-                    console.log(newCurrencies);
-                    commit('SET_LAST_DATE', new Date());
-                    // обновление кэша
-                    commit('SET_CURRENCIES', newCurrencies);
+                    let newCurrencies = await provider.get('/daily_json.js');
+                    if (newCurrencies && newCurrencies.data && newCurrencies.data.Valute) {
+                        newCurrencies = Object.values(newCurrencies.data.Valute);
+                        commit('SET_LAST_DATE', new Date());
+                        commit('SET_CURRENCIES', newCurrencies);        // update cache
+                    }
                 }
             } catch (err) {
-                // ошибка сети
+                // network error
                 if (err.message && err.message === 'Network Error') {
                     commit('SET_ERROR', err.message);
                 }
-                // другие ошибки
+                // other errors
                 else {
                     commit('SET_ERROR', err.response.data);
                     commit('CLEAR_CACHE');
