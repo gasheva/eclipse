1609674773a8183e19dd0890646c5c37950b1555
Татьяна Гашева <tg@get-net.ru>
2022-03-10 06:55:00 +0300

    feat: alter footbar style. Make footbar functional

--- client/src/components/app/Footbar.vue
+++ client/src/components/app/Footbar.vue
@@ -1,9 +1,3 @@
-<template>
-    <a href="https://www.cbr-xml-daily.ru/">Виджет курсов валют</a>
+<template functional>
+    <a href="https://www.cbr-xml-daily.ru/" class="black-text">Виджет курсов валют</a>
 </template>
-
-<style scoped>
-a {
-    color: #000;
-}
-</style>
