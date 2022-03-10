76838a56a944a057cd1f2aee508eec050c20e4af
Татьяна Гашева <tg@get-net.ru>
2022-03-10 08:11:58 +0300

    style: add comment

--- client/src/store/currency.js
+++ client/src/store/currency.js
@@ -38,17 +38,17 @@ export default {
                 // checking cache
                 // if dates coincide & cache exists then take from cache
                 if (getters.currencies && getters.lastDateUpdate && (new Date(getters.lastDateUpdate)).toDateString() === new Date().toDateString()) {
-                    console.log('Cash is relevant');
-                } else {
-                    // if cache is irrelevant
-                    commit('CLEAR_CACHE');
-                    let newCurrencies = await provider.get('/daily_json.js');
-                    if (newCurrencies && newCurrencies.data && newCurrencies.data.Valute) {
-                        newCurrencies = Object.values(newCurrencies.data.Valute);
-                        commit('SET_LAST_DATE', new Date());
-                        commit('SET_CURRENCIES', newCurrencies);        // update cache
-                    }
+                    return;
                 }
+                // if cache is irrelevant
+                commit('CLEAR_CACHE');
+                let newCurrencies = await provider.get('/daily_json.js');
+                if (newCurrencies && newCurrencies.data && newCurrencies.data.Valute) {
+                    newCurrencies = Object.values(newCurrencies.data.Valute);
+                    commit('SET_LAST_DATE', new Date());            // cbr updates exchange rate only once a day
+                    commit('SET_CURRENCIES', newCurrencies);        // update cache
+                }
+
             } catch (err) {
                 // network error
                 if (err.message && err.message === 'Network Error') {
